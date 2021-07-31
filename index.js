322ea0e3a38497189a8a8a76394e2052f80813e7
eko733 <49372345+eko733@users.noreply.github.com>
2021-08-01 00:55:43 +0700

    fix done

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function getTrade() {
               // NOthing here
             }else{
 
-              bigorder.push(`🔥  ' @langsidd#3455 ' Big Trade Alert! someone ${text_side} ${parseFloat(fill.size)} $BOP @${fill.price}`);
+              bigorder.push(`🔥  <@!826893342368333826> Big Trade Alert! someone ${text_side} ${parseFloat(fill.size)} $BOP @${fill.price}`);
               arr_donepost.push(`${fill.orderId}`)
               console.log(fill.eventFlags)
             }
