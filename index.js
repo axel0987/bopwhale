5e0b3e59d9ee0b797d801bbe57a0cd59f61cda49
Steve axel <49372345+eko733@users.noreply.github.com>
2021-08-02 04:38:24 +0700

    fix done

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ async function getTrade() {
               // NOthing here
             }else{
 
-              bigorder.push(`🔥  <@!823620797825417256> Ada yang beli!${text_side} ${parseFloat(fill.size)} $BOP @${fill.price} USDC`);
+              bigorder.push(`🔥  <@!823620797825417256> Ada yang beli!${text_side} ${parseFloat(fill.size)} $BOP || <@!444860158619942932> pertokennya @${fill.price} USDC`);
               arr_donepost.push(`${fill.orderId}`)
               console.log(fill.eventFlags)
             }
