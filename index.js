5021082331e13344f94d77e449daec4c138bebe8
Anom <49372345+eko733@users.noreply.github.com>
2021-08-18 18:57:39 +0700

    Fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let connection = new Connection('https://api.mainnet.rpcpool.com/');
 let programId = new PublicKey('9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin'); // Serum program v3
 let marketAddress = new PublicKey('8x8jf7ikJwgP9UthadtiGFgfFuyyyYPHL3obJAuxFWko'); //NINJA MARKET
 
-let treshold = "50";
+let treshold = "60";
 let arr_donepost = Array();
 
 
@@ -26,7 +26,7 @@ client.on('ready', async () => {
         // console.log(val)
         client.channels.cache.get(`${channelid}`).send(`${val}`)
     }
-  }, 50);
+  }, 60);
 
     client.user.setActivity(`${ await getOrder()}`, {
       type: "WATCHING"
